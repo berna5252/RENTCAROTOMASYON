005176d619a46501755e8cbbb956b9d414169795
berna5252 <berna.2006.aldemir@gmail.com>
2026-05-22 14:17:09 +0300

    Kiralama formu arayüzü güncellendi.

--- Form_CustomerCar.Designer.cs
+++ Form_CustomerCar.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_CustomerCar));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btn_listele = new System.Windows.Forms.Button();
             this.btn_ekle = new System.Windows.Forms.Button();
@@ -52,19 +53,21 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(2, 1);
+            this.dataGridView1.Location = new System.Drawing.Point(-181, -2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1601, 201);
+            this.dataGridView1.Size = new System.Drawing.Size(1810, 208);
             this.dataGridView1.TabIndex = 0;
             // 
             // btn_listele
             // 
-            this.btn_listele.Location = new System.Drawing.Point(588, 299);
+            this.btn_listele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_listele.Location = new System.Drawing.Point(588, 301);
             this.btn_listele.Name = "btn_listele";
-            this.btn_listele.Size = new System.Drawing.Size(112, 23);
+            this.btn_listele.Size = new System.Drawing.Size(112, 37);
             this.btn_listele.TabIndex = 1;
             this.btn_listele.Text = "LİSTELE";
             this.btn_listele.UseVisualStyleBackColor = true;
@@ -72,9 +75,10 @@
             // 
             // btn_ekle
             // 
+            this.btn_ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btn_ekle.Location = new System.Drawing.Point(588, 390);
             this.btn_ekle.Name = "btn_ekle";
-            this.btn_ekle.Size = new System.Drawing.Size(112, 23);
+            this.btn_ekle.Size = new System.Drawing.Size(112, 38);
             this.btn_ekle.TabIndex = 2;
             this.btn_ekle.Text = "EKLE";
             this.btn_ekle.UseVisualStyleBackColor = true;
@@ -82,9 +86,10 @@
             // 
             // btn_sil
             // 
+            this.btn_sil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btn_sil.Location = new System.Drawing.Point(588, 475);
             this.btn_sil.Name = "btn_sil";
-            this.btn_sil.Size = new System.Drawing.Size(112, 23);
+            this.btn_sil.Size = new System.Drawing.Size(112, 28);
             this.btn_sil.TabIndex = 3;
             this.btn_sil.Text = "SİL";
             this.btn_sil.UseVisualStyleBackColor = true;
@@ -93,18 +98,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Location = new System.Drawing.Point(44, 267);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "MÜŞTERİ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.Location = new System.Drawing.Point(44, 362);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.Size = new System.Drawing.Size(66, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "ARABA";
             // 
@@ -126,6 +136,7 @@
             // 
             // btn_form_customer
             // 
+            this.btn_form_customer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btn_form_customer.Location = new System.Drawing.Point(164, 631);
             this.btn_form_customer.Name = "btn_form_customer";
             this.btn_form_customer.Size = new System.Drawing.Size(172, 39);
@@ -151,30 +162,38 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.Location = new System.Drawing.Point(44, 440);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 16);
+            this.label3.Size = new System.Drawing.Size(104, 20);
             this.label3.TabIndex = 11;
             this.label3.Text = "ALIŞ TARİHİ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label4.Location = new System.Drawing.Point(44, 534);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(102, 16);
+            this.label4.Size = new System.Drawing.Size(128, 20);
             this.label4.TabIndex = 12;
             this.label4.Text = "TESLİM TARİHİ";
             // 
             // groupBox1
             // 
+            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.groupBox1.Controls.Add(this.lbl_1);
             this.groupBox1.Controls.Add(this.lbl_2);
             this.groupBox1.Controls.Add(this.rd_2);
             this.groupBox1.Controls.Add(this.rd_1);
-            this.groupBox1.Location = new System.Drawing.Point(794, 235);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(806, 301);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(661, 315);
+            this.groupBox1.Size = new System.Drawing.Size(661, 447);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "RAPORLAR";
@@ -182,54 +201,63 @@
             // lbl_1
             // 
             this.lbl_1.AutoSize = true;
-            this.lbl_1.Location = new System.Drawing.Point(323, 39);
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_1.Location = new System.Drawing.Point(386, 44);
             this.lbl_1.Name = "lbl_1";
-            this.lbl_1.Size = new System.Drawing.Size(11, 16);
+            this.lbl_1.Size = new System.Drawing.Size(16, 22);
             this.lbl_1.TabIndex = 14;
             this.lbl_1.Text = "-";
             // 
             // lbl_2
             // 
             this.lbl_2.AutoSize = true;
-            this.lbl_2.Location = new System.Drawing.Point(323, 157);
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_2.Location = new System.Drawing.Point(386, 155);
             this.lbl_2.Name = "lbl_2";
-            this.lbl_2.Size = new System.Drawing.Size(11, 16);
+            this.lbl_2.Size = new System.Drawing.Size(16, 22);
             this.lbl_2.TabIndex = 15;
             this.lbl_2.Text = "-";
             // 
             // rd_2
             // 
             this.rd_2.AutoSize = true;
+            this.rd_2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rd_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.rd_2.Location = new System.Drawing.Point(6, 155);
             this.rd_2.Name = "rd_2";
-            this.rd_2.Size = new System.Drawing.Size(204, 20);
+            this.rd_2.Size = new System.Drawing.Size(255, 24);
             this.rd_2.TabIndex = 1;
             this.rd_2.TabStop = true;
             this.rd_2.Text = "EN ÇOK KİRALANAN ARABA";
-            this.rd_2.UseVisualStyleBackColor = true;
+            this.rd_2.UseVisualStyleBackColor = false;
             this.rd_2.CheckedChanged += new System.EventHandler(this.rd_2_CheckedChanged);
             // 
             // rd_1
             // 
             this.rd_1.AutoSize = true;
+            this.rd_1.BackColor = System.Drawing.Color.White;
+            this.rd_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.rd_1.Location = new System.Drawing.Point(6, 44);
             this.rd_1.Name = "rd_1";
-            this.rd_1.Size = new System.Drawing.Size(259, 20);
+            this.rd_1.Size = new System.Drawing.Size(322, 24);
             this.rd_1.TabIndex = 0;
             this.rd_1.TabStop = true;
             this.rd_1.Text = "EN ÇOK KİRALAMA YAPAN MÜŞTERİ";
-            this.rd_1.UseVisualStyleBackColor = true;
+            this.rd_1.UseVisualStyleBackColor = false;
             this.rd_1.CheckedChanged += new System.EventHandler(this.rd_1_CheckedChanged);
             // 
             // Form_CustomerCar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1605, 737);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dtp_return_date);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.dtp_rent_date);
             this.Controls.Add(this.btn_form_customer);
             this.Controls.Add(this.cmb_car);
@@ -239,7 +267,6 @@
             this.Controls.Add(this.btn_sil);
             this.Controls.Add(this.btn_ekle);
             this.Controls.Add(this.btn_listele);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "Form_CustomerCar";
             this.Text = "Form_CustomerCar";
             this.Load += new System.EventHandler(this.Form_CustomerCar_Load);
